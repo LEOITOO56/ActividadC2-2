9d96208b64144339789a2ce9017a1d33988fc954
EDISON LEONARDO RAMIREZ ANGULO <85971636+LEOITOO56@users.noreply.github.com>
2021-07-16 03:01:16 -0500

    Create Principal.cpp
    Realice  el punto 4 y el punto 5, donde aplique lo aprendido, algunos código lo tuve que observar en YouTube o en googlees, para así poder terminar mi trabajo.

--- Principal.cpp
+++ Principal.cpp
@@ -10,64 +10,53 @@ int main()
 	
 	int da, ma, aa, an, mn, dn, a,m , d;
 	float peso,estatura,imc;
-	Persona Kerly;
+	Persona PEPE;
 	int op,flag;
 
 	do
 	{
-		cout<<"========================================================================"<<endl<<endl;
-	    cout<<"                     BIENVENIDO AL MENU DEL USUARIO\n";
-	    cout<<"                          - DATOS PERSONALES -"<<endl<<endl;
-	    cout<<"========================================================================"<<endl<<endl;
-		cout<<"[1] Ingresar y mostrar los datos personales. \n";
-		cout<<"[2] Calcular la edad de la persona. \n";
-		cout<<"[3] Mostrar el indice de masa corporal. \n"; 
-		cout<<"[4] Guardar archivo. \n";
-		cout<<"[5] Recuperar los datos desde el archivo .txt y mostrarlos por pantalla. \n";
-		cout<<"\n[0] Salir del menu. \n";
-		cout<<"\n========================================================================"<<endl<<endl;
-    	cout<<"Por favor ingrese una opcion : ";
+		cout<<"||----Datos Personales----||"<<endl<<endl;
+		cout<<"1.-Ingresar y mostrar los datos personales \n";
+		cout<<"2.-Calcular la edad de la personas \n";
+		cout<<"3.-Mostrar el indice de masa comporal \n "; 
+		cout<<"4.-Guardar archivo \n";
+		cout<<"5.-Recuperar los datos desde el archivo txt y mostrarlos por pantalla \n";
+		cout<<"0.-Salir del menu \n";
+    	cout<<"Por favor ingrese una opcion : "; 
 		cin>>op;
 		system("cls");
 		switch(op)
 		{
 			case 1:
-			Kerly.ingresar();
-			Kerly.mostrar();
+			PEPE.ingresar();
+			PEPE.mostrar();
 			system("pause");
 			break;
-			
-			cout<<"La edad que tiene es: ";
+			cout<<"La edad que tiene es:";
 	    	case 2:
-	    	cout<<"=============================================================="<<endl<<endl;
-	        cout<<"                    - CALCULO DE EDAD -\n";
-	        cout<<"\n==============================================================\n"<<endl<<endl;
-	    	cout<<"Por favor ingrese la fecha actual (AA/MM/DD) : "; 
+	    	cout<<"-----||CALCULO DE EDAD||-----"<<endl;	
+	    	cout<<"Por favor ingrese la fecha actual (AA/MM/DD): \n"; 
         	cin>>aa>>ma>>da;
-	    	Kerly.edad(aa,ma,da);
-	    	Kerly.edad2();
-			system("pause");
+	    	PEPE.edad( aa,ma,da);
+	    	PEPE.edad2();	
+			system("pause");	
 			break;
-			
 			case 3:
-			Kerly.IMC(imc);
+			PEPE.IMC(imc);
 			system("pause");
 			break;
-			
 			case 4:
-			Kerly.guardar();
+			PEPE.guardar();
 			system("pause");
 			break;
-			
 			case 5:
-	    	Kerly.rym();
+	    	PEPE.rym();
 	    	system("pause");
 			break;
-			
 		}
 		if(op>5)
 		{
-			cout << "Opcion no permitida!\n" <<  "Presione Enter para continuar\n";
+			cout << "Opcion no permitida !\n" <<  "Press Enter to continue\n";
    			// pause the program until user input
 		    flag=cin.ignore().get();
 		}		  
